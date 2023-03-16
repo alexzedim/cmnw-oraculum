0281f398ae3f83043495655b9a0229c10bbd4c1f
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2023-03-16 21:09:56 +0300

    feat(libs): add utils to shared

---
+++ libs/shared/src/utils/fefenya.utils.ts
@@ -0,0 +1,20 @@
+/**
+ * @description Gives a random int number in-between requested values
+ * @param min {number} Minimal number
+ * @param max {number} Maximum number
+ */
+export const randInBetweenInt = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+/**
+ * @description Generate random greeting for GotD command
+ * @param greeting {string} greeting flow string
+ * @param userId {string} id of discord guild member
+ * */
+export const gotdGreeter = (greeting: string, userId: string) =>
+  `${greeting} <@${userId}>`;
+
+export const gotdSelected = (greeting: string, username: string) =>
+  `${greeting} ${username}`;
+
+export const fefenyaKeyFormatter = (key: string) => `FEFENYA:${key}`;

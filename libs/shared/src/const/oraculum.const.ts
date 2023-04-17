142909e5687e03a716d25e0641c7abb57b271f56
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2023-04-17 22:29:26 +0300

    feat(shared): routing key for pepa

--- libs/shared/src/const/oraculum.const.ts
+++ libs/shared/src/const/oraculum.const.ts
@@ -1,2 +1,9 @@
+import { PEPA_TRIGGER_FLAG } from '@cmnw/shared/enums';
+
 export const ORACULUM_EXCHANGE = 'oraculum';
 export const MESSAGE_QUEUE = 'message';
+
+export const ROUTING_KEY = [
+  PEPA_TRIGGER_FLAG.MESSAGE_PROVOKE,
+  PEPA_TRIGGER_FLAG.MESSAGE_REPLY,
+];

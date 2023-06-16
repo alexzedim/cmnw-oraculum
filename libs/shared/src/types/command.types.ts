be6e9ed0cdd04391308108a267931390f60a5efa
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2023-06-16 14:09:08 +0300

    fix(shared): axios interface

--- libs/shared/src/types/command.types.ts
+++ libs/shared/src/types/command.types.ts
@@ -5,14 +5,12 @@ import { Logger } from '@nestjs/common';
 import { StorageTypes } from '@cmnw/shared/types/storage.types';
 import { Repository } from 'typeorm';
 import { FefenyaUsersEntity, PepaIdentityEntity } from '@cmnw/pg';
-import { HttpService } from '@nestjs/axios';
 
 export interface ISlashCommandArgs {
   readonly interaction: Interaction;
   readonly logger: Logger;
   readonly repository?: Repository<FefenyaUsersEntity | PepaIdentityEntity>; // TODO as repo storage
   readonly localStorage?: StorageTypes;
-  readonly httpModule?: HttpService;
   readonly redis?: Redis;
 }
 

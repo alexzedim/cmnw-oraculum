25b52d31b418d3702af1b9a88062d58227922812
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2023-12-30 15:50:02 +0300

    feat(libs): extend const commands

--- libs/commands/src/const/commands.const.ts
+++ libs/commands/src/const/commands.const.ts
@@ -1,8 +1,29 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 
-export const VOTING_QUORUM = {
+export const VOTING_ACTION = new Map<string, string>([
+  ['1', 'назначение на роль'],
+  ['2', 'освобождение от указанной роли'],
+  ['3', 'присвоение роли'],
+  ['4', 'лишение роли'],
+]);
 
-}
+export const buildVotingAction = (
+  userId: string,
+  roleId: string,
+  isDemoted: boolean,
+) => {
+  const action = {
+    title: `Голосование за присвоение роли`,
+    description: `Назначить <@${userId}> на роль <@&${roleId}>`,
+  };
+
+  if (isDemoted) {
+    action.title = `Голосование за освобождение от указанной роли`;
+    action.description = `Освободить участника сообщества <@${userId}> от роли <@&${roleId}>`;
+  }
+
+  return action;
+};
 
 export enum VOTING_NOMINATION_ENUM {
   NAME = 'vote-nomination',
@@ -24,5 +45,21 @@ export const VOTING_NOMINATION = new SlashCommandBuilder()
     option
       .setName(VOTING_NOMINATION_ENUM.NAME_OPTION)
       .setDescription('участник сообщества')
+      .setRequired(true),
+  );
+
+export enum IDENTITY_ENUM {
+  NAME = 'identity',
+  DESCRIPTION = 'Привязать выбранную сущность',
+  PROFILE_OPTION = 'профиль',
+}
+
+export const IDENTITY = new SlashCommandBuilder()
+  .setName(IDENTITY_ENUM.NAME)
+  .setDescription(IDENTITY_ENUM.DESCRIPTION)
+  .addStringOption((option) =>
+    option
+      .setName(IDENTITY_ENUM.PROFILE_OPTION)
+      .setDescription('профиль')
       .setRequired(true),
   );
